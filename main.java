a6811a8f06541bd4e539cbf343edd79678ad4330
Olarn U <olarn.u@gmail.com>
2018-07-31 17:23:16 +0700

    Update olarn's file and add main code.

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public static void Main(String [] args) {
-	System.out.println("Hello, World");
-	System.out.println("Hello, World 2");
 	Home home = new Home();
 	Menu menu = new Menu();
+	Olarn olarn = new Olarn();
+	System.out.println(olarn.getName());
 }
