05fd36eed490550cffd048e1c6802cdafca4fb8a
Witchuda Chobchuenchom <nan.garfeild@gmail.com>
2018-07-31 17:58:56 +0700

    modify main file

--- main.java
+++ main.java
@@ -3,6 +3,7 @@ public static void Main(String [] args) {
 	Menu menu = new Menu();
 	Nan nan = new Nan();
 	system.out.println("getBello : " + nan.getBello);
+	system.out.println("I'm Nan");
 	Olarn olarn = new Olarn();
 	System.out.println(olarn.getName());
 }
